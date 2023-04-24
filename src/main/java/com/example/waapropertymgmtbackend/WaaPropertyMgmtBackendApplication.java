3e01effba12ac384836af4097dd4966fecba12a7
Kaleb Wondwossen <kaleb.wond@gmail.com>
2023-04-24 13:15:13 -0500

    init

---
+++ src/main/java/com/example/waapropertymgmtbackend/WaaPropertyMgmtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.waapropertymgmtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaPropertyMgmtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaaPropertyMgmtBackendApplication.class, args);
+	}
+
+}
