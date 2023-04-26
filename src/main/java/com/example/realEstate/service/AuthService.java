af386b33be6179bae035676585370ff0b495b717
kzihon <kzihon@miu.edu>
2023-04-26 13:41:00 -0500

    Security layer added

---
+++ src/main/java/com/example/realEstate/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.example.realEstate.service;
+
+import com.example.realEstate.entity.dto.request.LoginRequest;
+import com.example.realEstate.entity.dto.request.RefreshTokenRequest;
+import com.example.realEstate.entity.dto.response.LoginResponse;
+
+public interface AuthService {
+    LoginResponse login(LoginRequest loginRequest);
+    LoginResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
+}
